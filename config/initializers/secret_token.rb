f12278b94ab4a7fc7e07dedb3c811cce0f0645aa
Paul D <paul.dacus@g5searchmarketing.com>
2011-06-23 09:52:23 -0700

    Initial commit of g5cms2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+G5cms::Application.config.secret_token = 'e2278fda06752dc24acf98fcedc740952823ee5ee1a5c34e56442c630f53cea5717c7791ce57a35df0762b475b70a6779a3fc8edf98a6389bab53412818e6fc4'
